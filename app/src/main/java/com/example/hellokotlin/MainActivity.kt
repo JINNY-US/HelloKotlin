42607af33a90d952b63a938ac2a3fd0d9356c606
JINNY.US <kyungjin2215@gmail.com>
2021-04-28 14:47:35 +0900

    Button with counting clicks & start activity time

--- app/src/main/java/com/example/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/example/hellokotlin/MainActivity.kt
@@ -14,18 +14,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var intent : Intent
-
-        val btnKotlin = findViewById<Button>(R.id.btnKotlin)
-        btnKotlin.setOnClickListener {
-            intent = Intent(this@MainActivity, BmiKotlinActivity::class.java)
-            startActivity(intent)
-        }
+        //var intent : Intent
 
         val btnJava = findViewById<Button>(R.id.btnJava)
+        val btnKotlin = findViewById<Button>(R.id.btnKotlin)
+
+        val btnVarJava = findViewById<Button>(R.id.btnVarJava)
+        val btnVarKotlin = findViewById<Button>(R.id.btnVarKotlin)
+
         btnJava.setOnClickListener {
-            intent = Intent(this@MainActivity, BmiJavaActivity::class.java)
-            startActivity(intent)
+            //intent = Intent(this@MainActivity, BmiJavaActivity::class.java)
+            startActivity(Intent(this@MainActivity, BmiJavaActivity::class.java)
+            )
+        }
+        btnKotlin.setOnClickListener {
+            //intent = Intent(this@MainActivity, BmiKotlinActivity::class.java)
+            startActivity(Intent(this@MainActivity, BmiKotlinActivity::class.java)
+            )
+        }
+
+        btnVarJava.setOnClickListener {
+            //intent = Intent(this@MainActivity, VariableJavaActivity::class.java)
+            startActivity(Intent(this@MainActivity, VariableJavaActivity::class.java)
+            )
+        }
+        btnVarKotlin.setOnClickListener {
+            //intent = Intent(this@MainActivity, VariableKotlinActivity::class.java)
+            startActivity(Intent(this@MainActivity, VariableKotlinActivity::class.java)
+            )
         }
     }
 }
